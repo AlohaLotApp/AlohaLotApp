b44e796fae28c43eb577276263d8d2f22be5d42d
AndrewDem <andreasdemirtz@gmail.com>
2025-05-24 12:20:12 +0300

    Small changes in sing up and conection with the data base

--- app/src/main/java/com/example/alohalotapp/AdminMainActivity.java
+++ app/src/main/java/com/example/alohalotapp/AdminMainActivity.java
@@ -1,6 +1,8 @@
 package com.example.alohalotapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,19 +13,22 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import androidx.appcompat.widget.SearchView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-
 
 public class AdminMainActivity extends AppCompatActivity {
 
     ListView listView;
-    ArrayList<String> names = new ArrayList<>();
+    ArrayList<String> parkingNames = new ArrayList<>();
     ArrayAdapter<String> arrayAdapter;
+
     DatabaseReference parkingRef;
 
     @Override
@@ -32,60 +37,63 @@ public class AdminMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_main);
 
         listView = findViewById(R.id.listView);
+        Button createNewButton = findViewById(R.id.createNewButton);
 
-        arrayAdapter = new ArrayAdapter<String>(this , android.R.layout.simple_dropdown_item_1line , names);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, parkingNames);
         listView.setAdapter(arrayAdapter);
 
+        // Firebase reference
         parkingRef = FirebaseDatabase.getInstance("https://alohalot-e2fd9-default-rtdb.asia-southeast1.firebasedatabase.app/")
-                .getReference("ParkingSpaces");
+                .getReference("ParkingSpaces`");
 
-        Button createNewButton = findViewById(R.id.createNewButton);
-        createNewButton.setOnClickListener(new View.OnClickListener() {
+        // Load parking names from Firebase
+        parkingRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AdminMainActivity.this,CreateNewParkingActivity.class);
-                startActivity(intent);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                parkingNames.clear();
+                for (DataSnapshot childSnapshot : snapshot.getChildren()) {
+                    String name = childSnapshot.child("name").getValue(String.class);
+                    if (name != null) {
+                        parkingNames.add(name);
+                    }
+                }
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(AdminMainActivity.this, "Failed to load data", Toast.LENGTH_SHORT).show();
             }
         });
 
-        // Handle item click
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String selectedName = names.get(position);
+        createNewButton.setOnClickListener(v -> {
+            Intent intent = new Intent(AdminMainActivity.this, CreateNewParkingActivity.class);
+            startActivity(intent);
+        });
 
-                Intent intent = new Intent(AdminMainActivity.this, ModifyParkingActivity.class);
-                intent.putExtra("parking_name", selectedName);
-                startActivity(intent);
-            }
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedName = parkingNames.get(position);
+            Intent intent = new Intent(AdminMainActivity.this, ModifyParkingActivity.class);
+            intent.putExtra("parking_name", selectedName);
+            startActivity(intent);
         });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-
-        getMenuInflater().inflate(R.menu.adminmenu , menu);
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.adminmenu, menu);
         MenuItem menuItem = menu.findItem(R.id.action_search);
         SearchView searchView = (SearchView) menuItem.getActionView();
         searchView.setQueryHint("Type here to search");
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-
+            @Override public boolean onQueryTextSubmit(String query) { return false; }
+            @Override public boolean onQueryTextChange(String newText) {
                 arrayAdapter.getFilter().filter(newText);
-
                 return false;
             }
         });
 
         return super.onCreateOptionsMenu(menu);
-
     }
 }
