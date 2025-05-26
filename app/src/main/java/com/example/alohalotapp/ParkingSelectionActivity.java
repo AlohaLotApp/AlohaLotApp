2ebe35cd616979d690a482666466c236d3e86b8c
billavramidis <bavramidis04@gmail.com>
2025-05-26 23:22:18 +0300

    created a singleton class for map utilities

--- app/src/main/java/com/example/alohalotapp/ParkingSelectionActivity.java
+++ app/src/main/java/com/example/alohalotapp/ParkingSelectionActivity.java
@@ -8,11 +8,13 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.alohalotapp.admin.FirebaseAdminHelperClass;
+import com.example.alohalotapp.map.MapHelperClass;
 import com.squareup.picasso.Picasso;
 
 public class ParkingSelectionActivity extends AppCompatActivity {
     //temporary
     private FirebaseAdminHelperClass firebaseHelper;
+    private MapHelperClass mapHelper;
     private static final String STATIC_MAP_API_KEY = "DUMMY";
 
     @Override
@@ -22,39 +24,14 @@ public class ParkingSelectionActivity extends AppCompatActivity {
 
         ImageView map = findViewById(R.id.map);
         firebaseHelper = new FirebaseAdminHelperClass();
+        mapHelper = MapHelperClass.getInstance(map);
 
         map.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
                 map.getViewTreeObserver().removeOnGlobalLayoutListener(this);
 
-                int width = map.getWidth();
-                int height = map.getHeight();
-
-                firebaseHelper.loadCoordinates(coordinatesList -> {
-                    StringBuilder markerBuilder = new StringBuilder();
-                    markerBuilder.append("&markers=color:red%7Clabel:P%7C");
-
-                    for (int i = 0; i < coordinatesList.size(); i++) {
-                        String marker = coordinatesList.get(i).replace("&markers=color:red%7Clabel:P%7C", "");
-                        markerBuilder.append(marker);
-                        if (i != coordinatesList.size() - 1) {
-                            markerBuilder.append("%7C");
-                        }
-                    }
-
-                    String mapUrl = "https://maps.googleapis.com/maps/api/staticmap"
-                            + "?center=Honolulu,United+States"
-                            + "&zoom=13"
-                            + "&size=" + width + "x" + height
-                            + markerBuilder
-                            + "&key=" + STATIC_MAP_API_KEY;
-
-                    Picasso.get().load(mapUrl).into(map);
-
-                }, error -> {
-                    Toast.makeText(ParkingSelectionActivity.this, "Failed to load coordinates: " + error, Toast.LENGTH_LONG).show();
-                });
+                mapHelper.addMarkers(ParkingSelectionActivity.this);
             }
         });
     }
