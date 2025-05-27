070688c23c76f14d1e3d638d23d39820624c7d5c
StefaniaThessalonikiou <iis23072@uom.edu.gr>
2025-05-27 19:36:19 +0300

    added an import

--- app/src/main/java/com/example/alohalotapp/StartActivity.java
+++ app/src/main/java/com/example/alohalotapp/StartActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
