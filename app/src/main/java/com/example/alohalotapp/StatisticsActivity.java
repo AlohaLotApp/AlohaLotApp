9c8814964108896d744700fc5b75e4561c951176
KiriakiKad <kiriakikadrefi@gmail.com>
2025-05-27 20:07:58 +0300

    Initial commit

--- app/src/main/java/com/example/alohalotapp/StatisticsActivity.java
+++ app/src/main/java/com/example/alohalotapp/StatisticsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -30,6 +31,9 @@ public class StatisticsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_statistics);
 
         usersEmailTextView = findViewById(R.id.statsImageTView);
