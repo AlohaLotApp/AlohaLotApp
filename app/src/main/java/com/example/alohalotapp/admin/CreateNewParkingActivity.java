f54bd0032b94f0da60bd2a4124fd0751d8a0c351
Radow Mystak <165580810+radowing@users.noreply.github.com>
2025-05-28 23:49:56 +0300

    Removed status bar from Admin screens, started defining and making buttons rounded and small titles

--- app/src/main/java/com/example/alohalotapp/admin/CreateNewParkingActivity.java
+++ app/src/main/java/com/example/alohalotapp/admin/CreateNewParkingActivity.java
@@ -3,6 +3,7 @@ package com.example.alohalotapp.admin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,6 +20,7 @@ public class CreateNewParkingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_create_new_parking);
 
         firebaseHelper = new FirebaseAdminHelperClass(); // <- initialize helper
