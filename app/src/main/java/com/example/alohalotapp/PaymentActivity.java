e0a5bdd38cbbdd8cddc9185b172a554a55693296
billavramidis <bavramidis04@gmail.com>
2025-05-31 02:12:33 +0300

    changed order time to include date

--- app/src/main/java/com/example/alohalotapp/PaymentActivity.java
+++ app/src/main/java/com/example/alohalotapp/PaymentActivity.java
@@ -18,6 +18,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
@@ -164,10 +165,10 @@ public class PaymentActivity extends AppCompatActivity {
 
     private void addOrder(FirebaseDatabase database, double amountPaid){
         DatabaseReference orderRef = database.getReference("users").child(userId).child("orders");
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm dd-MM-yy");
 
         HashMap<Double, Integer> durationMap = new HashMap<>();
-        durationMap.put(3.0, 30); //3$ 30 minutes
+        durationMap.put(3.0, 30); //3$ 30 minutesx
         durationMap.put(5.0, 60); //5$ 1 hours
         durationMap.put(11.0, 180); //11$ 3 hours
 
@@ -183,8 +184,8 @@ public class PaymentActivity extends AppCompatActivity {
                 String parkingName = getIntent().getStringExtra("parkingName");
 
                 Map<String, Object> order = new HashMap<>();
-                order.put("arrivalTime", LocalTime.now().format(timeFormatter));
-                order.put("departureTime", LocalTime.now().plusMinutes(durationMap.get(amountPaid)).format(timeFormatter));
+                order.put("arrivalTime", LocalDateTime.now().format(timeFormatter));
+                order.put("departureTime", LocalDateTime.now().plusMinutes(durationMap.get(amountPaid)).format(timeFormatter));
                 order.put("parkingName", parkingName);
 
                 fireBaseHelper.getParkingSpaceByName(parkingName, parkingWithId -> {
